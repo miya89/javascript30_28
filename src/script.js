20e9a1e25bf601a41de9700bdb5ec4dfb63a3d50
MiyaPeynova <maria.peynova@hotmail.com>
2021-09-06 07:30:32 +0100

    Add speedControl func

--- src/script.js
+++ src/script.js
@@ -1,9 +1,17 @@
-const video = document.querySelector(".video");
+const video = document.querySelector("video");
 const speed = document.querySelector(".speed");
 const bar = speed.querySelector(".speed-bar");
 
 function speedControl(e) {
-  console.log(e);
+  const y = e.pageY - this.offsetTop;
+  const percent = y / this.offsetHeight;
+  const min = 0.4;
+  const max = 4;
+  const height = Math.round(percent * 100) + "%";
+  const playbackRate = percent * (max - min) + min;
+  bar.style.height = height;
+  bar.textContent = playbackRate.toFixed(2) + "×";
+  video.playbackRate = playbackRate;
 }
 
 speed.addEventListener("mousemove", speedControl);
